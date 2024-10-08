65e442d5277edb081e55519977f1a0a00a5c2827
João Guilherme <jzati@mitsidi.com>
2024-10-08 13:59:10 -0300

    Adicionando exemplo double para ver a variavel

---
+++ exercicios/174_processamentos_dados.cpp
@@ -0,0 +1,22 @@
+// inclui bibliotecass
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, resultado;
+    double resultado_2;
+
+    // mesmo processo de calculo que o c
+    a = 5;
+    b = 2;
+
+    resultado = a / b;
+    resultado_2 = (double) a / b;
+
+    // volta inteiro
+    cout << "Resultado Inteiro: " << resultado << endl;
+    cout << "Resultado Double: " << resultado_2 << endl;
+
+    return 0;
+};
